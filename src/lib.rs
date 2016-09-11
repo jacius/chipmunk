4f236f54aefbad656137cd67ba12976f4eeb2de5
John Croisant <john@croisant.net>
2016-09-11 15:32:44 -0500

    Removed `forward!` macro (no longer used).

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,6 @@
 extern crate chipmunk_sys as chip;
 extern crate void;
 
-macro_rules! forward {
-    // &self
-    ($name:ident <$($typearg:type),*> (&self, $($arg:ident : $typ:ty),*) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name <$($typearg),*> (&self, $($arg : $typ),*) -> $ret {
-            $(#![$doc])*
-
-            unsafe {
-                (*self.raw.get()).$name($($arg),*)
-            }
-        }
-    };
-    ($name:ident (&self, $($arg:ident : $typ:ty),*) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name (&self, $($arg : $typ),*) -> $ret {
-            $(#![$doc])*
-
-            unsafe {
-                (*self.raw.get()).$name($($arg),*)
-            }
-        }
-    };
-    ($name:ident (&self) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name (&self) -> $ret {
-            $(#![$doc])*
-            unsafe {
-                (*self.raw.get()).$name()
-            }
-        }
-    };
-
-    // &mut self
-    ($name:ident <$($typearg:type),*> (&mut self, $($arg:ident : $typ:ty),*) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name <$($typearg),*> (&mut self, $($arg : $typ),*) -> $ret {
-            $(#![$doc])*
-            unsafe {
-                (*self.raw.get()).$name($($arg),*)
-            }
-        }
-    };
-    ($name:ident (&mut self, $($arg:ident : $typ:ty),*) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name (&mut self, $($arg : $typ),*) -> $ret {
-            $(#![$doc])*
-            unsafe {
-                (*self.raw.get()).$name($($arg),*)
-            }
-        }
-    };
-    ($name:ident (&mut self) -> $ret:ty, $(#[$doc:meta])*) => {
-        pub fn $name (&mut self) -> $ret {
-            $(#![$doc])*
-            unsafe {
-                self.raw.get().$name()
-            }
-        }
-    };
-}
-
 pub mod arbiter;
 pub mod body;
 pub mod handle;
